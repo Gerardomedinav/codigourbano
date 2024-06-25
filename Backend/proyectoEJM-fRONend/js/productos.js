3c5efb6ff1ebfe5d67dfee3a8a5d1b75e66edf8b
felix11111 <11felixfenix11@gmail.com>
2024-06-25 14:21:03 -0300

    el backend denuevossss

---
+++ Backend/proyectoEJM-fRONend/js/productos.js
@@ -0,0 +1,90 @@
+const { createApp } = Vue
+createApp({
+    data() {
+        return {
+            productos: [],
+            url: 'http://127.0.0.1:5000/productos',
+            error: false,
+            cargando: true,
+            id: 0,
+            nombre: "",
+            imagen: "",
+            stock: 0,
+            tipoProducto: "",
+            precio: 0,
+            tiposFiltro: [],
+            tipoSeleccionado: ""
+        }
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.productos = data;
+                    this.cargando = false
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error = true
+                })
+        },
+        eliminar(id) {
+            const url = this.url + '/' + id;
+            var options = {
+                method: 'DELETE',
+            }
+            fetch(url, options)
+                .then(res => res.text())
+                .then(res => {
+                    alert('Registro Eliminado')
+                    location.reload();
+                })
+        },
+        grabar() {
+            let producto = {
+                nombre: this.nombre,
+                precio: this.precio,
+                stock: this.stock,
+                tipoProducto: this.tipoProducto,
+                imagen: this.imagen
+            }
+            var options = {
+                body: JSON.stringify(producto),
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow'
+            }
+            fetch(this.url, options)
+                .then(function () {
+                    alert("Registro grabado")
+                    window.location.href = "./productos.html";
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al Grabar")
+                })
+        },
+        filtrarPorTipo() {
+            if (this.tipoSeleccionado) {
+                this.fetchData(`${this.url}?tipo=${this.tipoSeleccionado}`);
+            } else {
+                this.fetchData(this.url);
+            }
+        },
+        obtenerTipos() {
+            fetch(`${this.url}/tipos`)
+                .then(response => response.json())
+                .then(data => {
+                    this.tiposFiltro = data;
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+        }
+    },
+    created() {
+        this.fetchData(this.url);
+        this.obtenerTipos();
+    },
+}).mount('#app')
