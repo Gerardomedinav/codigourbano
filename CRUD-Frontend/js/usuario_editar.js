76d85929bdeb20b769532c4cd5fabc57315d32b3
Salvatorum <tscanosa@gmail.com>
2024-07-02 14:04:08 -0300

    usuario_editar.html y usuario_editar.js

---
+++ CRUD-Frontend/js/usuario_editar.js
@@ -0,0 +1,60 @@
+console.log(location.search)     // lee los argumentos pasados a este formulario
+var id=location.search.substr(4)  // producto_update.html?id=1
+console.log(id)
+const { createApp } = Vue
+  createApp({
+    data() {
+      return {
+        id:0,
+        nombre:"",
+        contraseña:"",
+        email:"",
+        tipoUsuario:"",
+        url:'https://felixcanosa.pythonanywhere.com/usuarios/'+id,
+       }  
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    this.id=data.id
+                    this.nombre = data.nombre;
+                    this.contraseña=data.contrasena
+                    this.email=data.email
+                    this.tipoUsuario=data.tipo_usuario          
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error=true              
+                })
+        },
+        modificar() {
+            let usuario = {
+                nombre:this.nombre,
+                contraseña: this.contraseña,
+                email: this.email,
+                tipo_usuario: this.tipoUsuario ? 0 : 1
+            }
+            var options = {
+                body: JSON.stringify(usuario),
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow'
+            }
+            fetch(this.url, options)
+                .then(function () {
+                    alert("Registro modificado")
+                    window.location.href = "./usuarios.html"; // navega a usuarios.html          
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al Modificar")
+                })      
+        }
+    },
+    created() {
+        this.fetchData(this.url)
+    },
+  }).mount('#app')
